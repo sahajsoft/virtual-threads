38b0cf925015a99ccd849fdae1347a61d5a93472
Lohith KM <lohithkm@sahaj.ai>
2024-03-01 10:59:22 +0530

    Initialize project setup

---
+++ src/main/java/ai/sahaj/virtual/threads/VirtualThreadsWorkshopApplication.java
@@ -0,0 +1,13 @@
+package ai.sahaj.virtual.threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadsWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/sahaj/virtual/threads/VirtualThreadsWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.sahaj.virtual.threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadsWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
